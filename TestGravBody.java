8380ce3fd9fae2738b4ff90ba21b51e14f6f7362
Bujar Sefa <bujars0698@afsenyc.org>
2016-12-21 20:38:49 -0500

    Added five test cases for XCoord in Test

--- TestGravBody.java
+++ TestGravBody.java
@@ -5,11 +5,11 @@ public class TestGravBody {
     
     @Test
     public void testGetXCoord() {
-	GravBody gb = new GravBody(1, 0, 0, 0, 0, 0, 0, 0);
-	GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
+	GravBody gb = new GravBody(1.0, 1.0, 1.0, 1.0, 1.0, 1, 1, 1);
+ 	GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
 	GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
-        GravBody ge = new GravBody(20, 0, 0, 0, 0, 0, 0, 0);
-	GravBody gf = new GravBody(55, 0, 0, 0, 0, 0, 0, 0);
+        GravBody ge = new GravBody(-1.0, -1.0, -1.0, -1.0, -1.0, -1, -1, -1);
+	GravBody gf = new GravBody(100.0, 100.0, 100.0, 100.0, 100.0, 100, 100, 100);
 
 	// It is NEVER a good idea to check if floating point numbers are
         // ==. To represent floating point numbers using binary, the
@@ -20,82 +20,124 @@ public class TestGravBody {
 	// Instead, it's a good idea to see if floating point numbers are
 	// really really CLOSE. So we need to specify how close they should
         // be to basically count as equal. That's this number below:
+	
 	double delta = 0.00001;
 
 	assertEquals(1, gb.getXCoord(), delta);
 	assertEquals(0, gc.getXCoord(), delta);
 	assertEquals(2.95365, gd.getXCoord(), delta);
-	assertEquals(20, ge.getXCoord(), delta);
-	assertEquals(55, gf.getXCoord(), delta);
+	assertEquals(1.0, ge.getXCoord(), delta);
+	assertEquals(100, gf.getXCoord(), delta);
     }
 
 
     @Test
 	public void testGetYCoord() {
-	GravBody gb = new GravBody(1, 3, 0, 0, 0, 0, 0, 0); // TODO: Replace null with call to GravBody
-	GravBody gc = new GravBody(1, 3, 0, 0, 0, 0, 0, 0);
-	GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);	           
-         // constructor that sets the y-value to you like
+	GravBody gb = new GravBody(1.0, 1.0, 1.0, 1.0, 1.0, 1, 1, 1);
+        GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
+        GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
+        GravBody ge = new GravBody(-1.0, -1.0, -1.0, -1.0, -1.0, -1, -1, -1);
+        GravBody gf = new GravBody(100.0, 100.0, 100.0, 100.0, 100.0, 100, 100, 100);
+        // constructor that sets the y-value to you like
+	
 	double delta = 0.00001;
 	// TODO: assert that gb.getYCoord() returns the y-value you picked in
 	// the constructor. This is exactly like testGetXCoord() above.
-	assertEquals(3, gb.getYCoord(),delta);
-	assertEquals(1.52308, gd.getYCoord(), delta);  
+	
+	assertEquals(1, gb.getYCoord(), delta);
+        assertEquals(0, gc.getYCoord(), delta);
+        assertEquals(1.52308, gd.getYCoord(), delta);
+        assertEquals(–1, ge.getYCoord(), delta);
+        assertEquals(100, gf.getYCoord(), delta);
     }
 
     // TODO: Add a test for getXVel
     @Test
 	public void testGetXVel() {
 	//Basic Implementation that I added - BUBU
-	GravBody gb = new GravBody(1, 3, 6, 0, 0, 0, 0, 0);
-	GravBody gc = new GravBody(1, 3, 6, 0, 0, 0, 0, 0);
-	GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
-	GravBody ge = new GravBody(1, 3, 6, 0, 0, 0, 0, 0);
-	GravBody gf = new GravBody(1, 3, 6, 0, 0, 0, 0, 0);
+	GravBody gb = new GravBody(1, 1, 1, 1, 1, 1, 1, 1);
+        GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
+        GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
+        GravBody ge = new GravBody(-1, -1, -1, -1, -1, -1, -1, -1);
+        GravBody gf = new GravBody(100, 100, 100, 100, 100, 100, 100, 100);
+	
 	double delta = 0.00001;
-	assertEquals(1986.1203, gb.getXVel(), delta);
+	
+	assertEquals(1, gb.getXVel(), delta);
+	assertEquals(0, gc.getXVel(), delta);
 	assertEquals(-3.24931, gd.getXVel(), delta);
+        assertEquals(–1, ge.getXVel(), delta);
+	assertEquals(100, gf.getXVel(), delta);
     }
 
     // TODO: Add a test for getYVel
     @Test
 	public void testGetYVel() {
         //Basic Implementation that I added - BUBU                              
-        GravBody gb = new GravBody(1, 3, 6, 5, 0, 0, 0, 0);
-	GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
-        double delta = 0.00001;
-        assertEquals(6, gb.getYVel(), delta);
-	assertEquals(6.34897, gd.getYVel(), delta);
+	GravBody gb = new GravBody(1, 1, 1, 1, 1, 1, 1, 1);
+        GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
+        GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
+        GravBody ge = new GravBody(-1, -1, -1, -1, -1, -1, -1, -1);
+        GravBody gf = new GravBody(100, 100, 100, 100, 100, 100, 100, 100);
+        
+	double delta = 0.00001;
+	
+	assertEquals(1, gb.getYVel(), delta);
+        assertEquals(0, gc.getYVel(), delta);
+        assertEquals(6.34897, gd.getYVel(), delta);
+        assertEquals(–1, ge.getYVel(), delta);
+        assertEquals(100, gf.getYVel(), delta);
+
+
     }
 
     /*
     // TODO: Add a test for getRadius
     @Test
     public void testGetRadius() {
-        //Basic Implementation that I added - BUBU                              
-        GravBody gb = new GravBody();
+        //Basic Implementation that I added - BUBU
+        GravBody gb = new GravBody(1, 1, 1, 1, 1, 1, 1, 1);
+        GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
+        GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
+        GravBody ge = new GravBody(-1, -1, -1, -1, -1, -1, -1, -1);
+        GravBody gf = new GravBody(100, 100, 100, 100, 100, 100, 100, 100);
         double delta = 0.00001;
-        assertEquals(1986.1203, gb.getRadius(), delta);
+       
+
     }
         
     // TODO: Add a test for getRGB
     @Test
     public void testGetRGB() {                              
         //**Pull out each value, and then check if those values are equal//
-	GravBody gb = new GravBody();
+	GravBody gb = new GravBody(1, 1, 1, 1, 1, 1, 1, 1);
+        GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
+        GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
+        GravBody ge = new GravBody(-1, -1, -1, -1, -1, -1, -1, -1);
+        GravBody gf = new GravBody(100, 100, 100, 100, 100, 100, 100, 100);
         //double delta = 0.00001;
+
+
     }*/
     
     // TODO: Add a test for getMass
     @Test
 	public void testGetMass() {
-	    //Basic Implementation that I added - BUBU                              
-	GravBody gb = new GravBody(1, 3, 6, 5, 7, 0, 0, 0);
-	GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
+	//Basic Implementation that I added - BUBU                              
+	GravBody gb = new GravBody(1, 1, 1, 1, 1, 1, 1, 1);
+        GravBody gc = new GravBody(0, 0, 0, 0, 0, 0, 0, 0);
+        GravBody gd = new GravBody(2.95365, 1.52308, -3.24931, 6.34897, 5.96676, 255, 255, 0);
+        GravBody ge = new GravBody(-1, -1, -1, -1, -1, -1, -1, -1);
+        GravBody gf = new GravBody(100, 100, 100, 100, 100, 100, 100, 100);
 
 	double delta = 0.00001;
-	    assertEquals(5, gb.getMass(), delta);
-	    assertEquals(5.96676, gd.getMass(), delta);
+
+	assertEquals(1, gb.getMass(), delta);
+	assertEquals(0, gc.getMass(), delta);
+	assertEquals(5.96676, gd.getMass(), delta);
+        assertEquals(–1, ge.getMass(), delta);
+	assertEquals(100, gf.getMass(), delta);
+
 	}
    
     @Test
