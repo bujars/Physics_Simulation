4e768da1e507e2459782c22cb9599f21dfa12f82
Bujar Sefa <bujars0698@afsenyc.org>
2017-03-27 14:06:04 -0400

    Vector Files added

---
+++ Vector2DFiles-2/TestVector2D.java
@@ -0,0 +1,95 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class TestVector2D{
+
+    @Test
+    public void testGetMagnitude(){
+	Vector2D v0 = new Vector2D(0.0, 2.0);
+	assertEquals(2.0, v0.getMagnitude(), 1E-10);
+
+	Vector2D v1 = new Vector2D(-1.0, 0.0);
+	assertEquals(1.0, v1.getMagnitude(), 1E-10);
+
+	Vector2D v2 = new Vector2D(-4.0, -3.0);
+	assertEquals(5.0, v2.getMagnitude(), 1E-10);
+
+	Vector2D v3 = new Vector2D(-1.0, 2.0);
+	assertEquals(2.2360679775, v3.getMagnitude(), 1E-10);
+    }
+
+    @Test
+    public void testGetSum(){
+	Vector2D v00 = new Vector2D(4.0, 8.0);
+	Vector2D v01 = new Vector2D(-5.0, 3.0);
+	Vector2D actual0 = v00.getSum(v01);
+	Vector2D expect0 = new Vector2D(-1.0, 11.0);
+	String msg0 = "Actual: " + actual0 + "\n" +
+	    "is not close to\n" +
+	    "Expected: " + expect0;
+	assertTrue(msg0, actual0.isCloseTo(expect0, 1E-10));
+    }
+
+    @Test
+    public void testGetDiff(){
+	Vector2D v00 = new Vector2D(4.0, 8.0);
+	Vector2D v01 = new Vector2D(-5.0, 3.0);
+	Vector2D actual0 = v00.getDiff(v01);
+	Vector2D expect0 = new Vector2D(9.0, 5.0);
+	String msg0 = "Actual: " + actual0 + "\n" +
+	    "is not close to\n" +
+	    "Expected: " + expect0;
+	assertTrue(msg0, actual0.isCloseTo(expect0, 1E-10));
+    }
+
+    @Test
+    public void testGetScaling(){
+	Vector2D v0 = new Vector2D(4.0, 8.0);
+	Vector2D actual0 = v0.getScaling(0.5);
+	Vector2D expect0 = new Vector2D(2.0, 4.0);
+	String msg0 = "Actual: " + actual0 + "\n" +
+	    "is not close to\n" +
+	    "Expected: " + expect0;
+	assertTrue(msg0, actual0.isCloseTo(expect0, 1E-10));
+    }
+
+    @Test
+    public void testGetNormalization(){
+	Vector2D v0 = new Vector2D(3.0, -4.0);
+	Vector2D actual0 = v0.getNormalization();
+	Vector2D expect0 = new Vector2D(0.6, -0.8);
+	String msg0 = "Actual: " + actual0 + "\n" +
+	    "is not close to\n" +
+	    "Expected: " + expect0;
+	assertTrue(msg0, actual0.isCloseTo(expect0, 1E-10));
+
+	Vector2D v1 = new Vector2D(0.0, 0.0);
+	Vector2D actual1 = v1.getNormalization();
+	Vector2D expect1 = new Vector2D(0.0, 0.0);
+	String msg1 = "Actual: " + actual1 + "\n" +
+	    "is not close to\n" +
+	    "Expected: " + expect1;
+	assertTrue(msg1, actual1.isCloseTo(expect1, 1E-10));
+    }
+
+    @Test
+    public void testIsCloseTo(){
+	Vector2D v0 = new Vector2D(3.0, -4.0);
+	Vector2D v1 = new Vector2D(3.99, -3.01);
+	boolean actual0 = v0.isCloseTo(v1, 1.0);
+	String msg0 = "v0: " + v0 + "\n" +
+	    "is not considered close to\n" +
+	    "v1: " + v1 + "\n" +
+	    "within 1.0";
+	assertTrue(msg0, actual0);
+
+	boolean actual1 = v0.isCloseTo(v1, 0.9);
+	String msg1 = "v0: " + v0 + "\n" +
+	    "is considered close to\n" +
+	    "v1: " + v1 + "\n" +
+	    "within 0.9";
+	assertFalse(msg1, actual1);
+    }
+}
---
+++ Vector2DFiles-2/Vector2D.java
@@ -0,0 +1,125 @@
+public class Vector2D{
+
+    private double xComp;
+    private double yComp;
+
+    /**
+     * Constructs a new Vector2D object
+     * @param x the x-component of the vector
+     * @param y the y-component of the vector
+     */
+    public Vector2D(double x, double y){
+	xComp = x;
+	yComp = y;
+    }
+
+    /**
+     * Gets the magnitude/length of the vector
+     * @return the magnitude/length of the vector
+     */
+    public double getMagnitude(){
+	return Math.sqrt(xComp * xComp + yComp * yComp);
+    }
+
+    /**
+     * Gets the x-component of the vector
+     * @return the x-component of the vector
+     */
+    public double getXComp(){
+	return xComp;
+    }
+
+    /**
+     * Gets the y-component of the vector
+     * @return the y-component of hte vector
+     */
+    public double getYComp(){
+	return yComp;
+    }
+
+    /**
+     * Gets the sum/addition of two vectors. NOTE: This
+     * method DOES NOT MUTATE the Vector2D object.
+     * @param v an addend vector
+     * @return a NEW Vector2D object representing the
+     *         sum of the two vectors
+     */
+    public Vector2D getSum(Vector2D v){
+	return new Vector2D(this.xComp + v.xComp,
+			    this.yComp + v.yComp);
+    }
+
+    /**
+     * Gets the difference/subtraction of two vectors. 
+     * NOTE: This method DOES NOT MUTATE the Vector2D 
+     * object.
+     * @param v the subtrahend vector
+     * @return a NEW Vector2D object representing the
+     *         difference/subtration of the two vectors
+     */
+    public Vector2D getDiff(Vector2D v){
+	return new Vector2D(this.xComp - v.xComp,
+			    this.yComp - v.yComp);
+    }
+
+    /**
+     * Gets the vector that results from scaling this
+     * vector. NOTE: This method DOES NOT MUTATE the
+     * Vector2D object.
+     * @param scaleFactor the factor by which to scale
+     * @return a NEW Vector2D object representing the
+     *         result of scaling this vector.
+     */
+    public Vector2D getScaling(double scaleFactor){
+	return new Vector2D(this.xComp * scaleFactor,
+			    this.yComp * scaleFactor);
+    } 
+
+    /** 
+     * Gets the vector that results from normalizing
+     * this vector. NOTE: This method DOES NOT MUTATE
+     * the Vector2D object.
+     * @return a NEW Vector2D object representing the
+     *         result of normalizing this vector.
+     */
+    public Vector2D getNormalization(){
+	double mag = getMagnitude();
+	if(mag > 0.0){
+	    return new Vector2D(this.xComp/mag,
+				this.yComp/mag);
+	}
+	else{
+	    return new Vector2D(0.0, 0.0);
+	}
+	
+    }
+
+    /**
+     * Compares two vectors and returns true when both
+     * the x and y-components of the vectors are within
+     * delta of one another.
+     * @param v the comparison vector
+     * @param delta the maximum difference between the
+     *        x and y-compnents of the vectors such that
+     *        they are deemed "close to" one another 
+     * @returns true when the x and y-components of the
+     *          vectors are within delta of one another
+     */
+    public boolean isCloseTo(Vector2D v, double delta){
+	return (Math.abs(this.xComp - v.xComp) < delta) &&
+	    (Math.abs(this.yComp - v.yComp) < delta);	
+    }
+
+    /**
+     * Returns a String representation of this vector
+     * @return a String representation of this vector
+     */
+    public String toString(){
+	return this.getClass() + "\n" +
+	    "{x-component: " + xComp + ",\n" +
+	    "{y-component: " + yComp + "}";
+    }
+
+    
+
+}
